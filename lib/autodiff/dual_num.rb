c99b9c2503257610f2e6a7b318832ce862b901f6
John Lin <johnlin@exosite.com>
2019-03-22 12:15:05 +0800

    handles x + 1

--- lib/autodiff/dual_num.rb
+++ lib/autodiff/dual_num.rb
@@ -7,9 +7,19 @@ module Autodiff
       @epsilon = e
     end
 
+    def to_dual
+      self
+    end
+
     def +(other)
       r = @real + other.real
       e = @epsilon + other.epsilon
+      self.class.new(r, e)
+    end
+
+    def -(other)
+      r = @real - other.real
+      e = @epsilon - other.epsilon
       self.class.new(r, e)
     end
 
@@ -18,6 +28,23 @@ module Autodiff
       e = @real * other.epsilon + @epsilon * other.real
       self.class.new(r, e)
     end
+
+  end
+
+end
+
+class Numeric
+
+  def real
+    self
+  end
+
+  def epsilon
+    0
+  end
+
+  def to_dual
+    Autodiff::DualNum.new(self)
   end
 
 end
--- test/autodiff_test.rb
+++ test/autodiff_test.rb
@@ -9,19 +9,22 @@ class AutodiffTest < Minitest::Test
     refute_nil ::Autodiff.public_class_method(:gradient)
   end
 
-  def x_square(x)
-    x * x
-  end
-
-  def x_power_3(x)
-    x * x * x
+  def test_gradient_can_handle_x_plus_1
+    assert_equal 1, Autodiff.gradient(100) { |x| x + 1}
   end
 
   def test_gradient_can_handle_x_square
-    assert_equal 2, Autodiff.gradient(1) { |x| x_square(x)}
+    assert_equal 2, Autodiff.gradient(1) { |x| x*x}
   end
 
   def test_gradient_can_handle_x_power_3
-    assert_equal 12, Autodiff.gradient(2) { |x| x_power_3(x)}
+    assert_equal 12, Autodiff.gradient(2) { |x| x*x*x}
+  end
+
+  def test_gradient_can_handle_x_2_plus_x_3
+    assert_equal 16, Autodiff.gradient(2) { |x| x*x + x*x*x}
+  end
+  def test_gradient_can_handle_x_2_minus_x_3
+    assert_equal -8, Autodiff.gradient(2) { |x| x*x - x*x*x}
   end
 end
